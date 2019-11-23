8da6f8d82690d9365f27f7fdfa23b8609c5eed09
abhishek <abhishekatholi@cet.ac.in>
2019-11-24 02:03:11 +0530

    change

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports.new_db = "mongodb://localhost:27017/students"
 module.exports.connect = (dbname) => {
     dbname.connect(dbname)
-    chalk.bgGreen(console.log("student database connected"))
+    chalk.bgGreen(console.log("student has been database connected"))
 }
